6344d15f27ab234ad775fd5e989ec99077f17746
ziadaboageza <ziadaboageza@gmail.com>
2026-01-02 04:43:34 +0300

    Compelte the First Part of task

---
+++ Task3/src/com/beanScopes/Draw3d.java
@@ -0,0 +1,8 @@
+package com.beanScopes;
+
+public class Draw3d {
+    public void draw(String shapeName)
+    {
+        System.out.println(String.format("Drawing 3d For %s" , shapeName));
+    }
+}
